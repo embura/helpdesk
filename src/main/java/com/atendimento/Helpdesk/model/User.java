28a9e6a3b36b600cdca69f1a202b12c6930f8485
Jefferson S Coelho <jcoelho@igcorp.com.br>
2018-01-04 10:04:29 -0200

    Iniciando projeto de Helpdesk

---
+++ src/main/java/com/atendimento/Helpdesk/model/User.java
@@ -0,0 +1,22 @@
+package com.atendimento.Helpdesk.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "users")
+public class User {
+	
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column
+	private Long id;	
+	
+	
+
+}
